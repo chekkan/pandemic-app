523cbae84f1955112289f9bd382a5d6845644fb9
Harish Babu <harish_babu@outlook.com>
2020-03-22 13:29:17 +0000

    fetch from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { useQuery } from "react-query";
 
 function App() {
+  const { status, data, error, isFetching } = useQuery("countries", () => {
+    async function run(url) {
+      let res = await fetch(url);
+      return await res.json();
+    }
+    return run(
+      "https://localhost:5001/countries?_sort=total_confirmed&_order=desc"
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {status === "loading" ? (
+        "Loading..."
+      ) : status === "error" ? (
+        <span>Error: {error.message}</span>
+      ) : (
+        <div>
+          {data.map(country => (
+            <p key={country.id}>
+              {country.name}: {country.totalConfirmed} : {country.totalDeath} :{" "}
+              {country.totalRecovered}
+            </p>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
