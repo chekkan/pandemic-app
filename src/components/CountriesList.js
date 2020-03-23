132ea677d7ce9a69764cb700ef59bca6fa4da881
Harish Babu <harish_babu@outlook.com>
2020-03-23 00:14:34 +0000

    paginated country list

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -6,19 +6,31 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
+import TablePagination from "@material-ui/core/TablePagination";
 
-import { useQuery } from "react-query";
+import { usePaginatedQuery } from "react-query";
 
 function CountriesList() {
-  const { status, data, error } = useQuery("countries", () => {
-    async function run(url) {
-      let res = await fetch(url);
-      return await res.json();
+  const [page, setPage] = React.useState(0);
+
+  const { status, resolvedData, error } = usePaginatedQuery(
+    ["countries", page],
+    (key, page) => {
+      async function run(url) {
+        let res = await fetch(url);
+        return await res.json();
+      }
+
+      return run(
+        `https://pandemic-api.azurewebsites.net/v1/countries?_limit=15&_page=${page +
+          1}`
+      );
     }
-    return run(
-      "https://pandemic-api.azurewebsites.net/v1/countries?_sort=total_confirmed&_order=desc"
-    );
-  });
+  );
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
   return (
     <div>
@@ -27,36 +39,47 @@ function CountriesList() {
       ) : status === "error" ? (
         <span>Error: {error.message}</span>
       ) : (
-        <TableContainer component={Paper}>
-          <Table aria-label="countries">
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell align="right">Confirmed</TableCell>
-                <TableCell align="right">Deaths</TableCell>
-                <TableCell align="right">Recovered</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map(country => (
-                <TableRow key={country.id}>
-                  <TableCell component="th" scope="row">
-                    {country.name}
-                  </TableCell>
-                  <TableCell align="right">
-                    {country.confirmed.toLocaleString()}
-                  </TableCell>
-                  <TableCell align="right">
-                    {country.deaths.toLocaleString()}
-                  </TableCell>
-                  <TableCell align="right">
-                    {country.recovered.toLocaleString()}
-                  </TableCell>
+        <Paper>
+          <TableContainer>
+            <Table aria-label="countries">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="right">Confirmed</TableCell>
+                  <TableCell align="right">Deaths</TableCell>
+                  <TableCell align="right">Recovered</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {resolvedData.map(country => (
+                  <TableRow key={country.id}>
+                    <TableCell component="th" scope="row">
+                      {country.name}
+                    </TableCell>
+                    <TableCell align="right">
+                      {country.confirmed.toLocaleString()}
+                    </TableCell>
+                    <TableCell align="right">
+                      {country.deaths.toLocaleString()}
+                    </TableCell>
+                    <TableCell align="right">
+                      {country.recovered.toLocaleString()}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[15]}
+            component="div"
+            count={250}
+            rowsPerPage={15}
+            page={page}
+            onChangePage={handleChangePage}
+            // onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        </Paper>
       )}
     </div>
   );
